e1365cccaf6f9a6b629b26b04576c252e9ddb9cf
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-19 11:02:21 +0300

    SinglePostPage

--- src/pages/SinglePostPage.jsx
+++ src/pages/SinglePostPage.jsx
@@ -1,8 +1,13 @@
 function SinglePostPage() {
+  // gauti id to post kuriame esam
+
+  // parsiusti posta // https://github.com/CSFrequency/react-firebase-hooks/tree/v4.0.2/firestore#usedocument
+
   return (
     <div className="container">
       <h1 className="display-3 mt-5">SinglePostPage</h1>
       <p>This is SinglePostPage</p>
+      {/* single post */}
     </div>
   );
 }
