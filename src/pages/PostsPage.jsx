b44d2f4b87cbcc4414e496ff811bee2ec19fd55a
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-19 16:24:53 +0300

    server filter

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,4 +1,4 @@
-import { collection, orderBy, query } from 'firebase/firestore';
+import { collection, orderBy, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../firebase/firebase';
@@ -6,31 +6,55 @@ import SingleListPost from '../components/posts/SingleListPost';
 import Loader from '../components/ui/Loader/Loader';
 
 function PostsPage() {
-  const [filerArr, setFilerArr] = useState('css');
+  const [filerVal, setFilerVal] = useState('all');
   // parsiusti postus
   const postCollRef = collection(db, 'hookPosts');
-  const q = query(postCollRef, orderBy('author', 'desc'));
+
+  // const q = query(postCollRef, orderBy('author', 'desc'));
+  const q =
+    filerVal === 'all'
+      ? query(postCollRef)
+      : query(postCollRef, where('tags', 'array-contains', filerVal));
+
   const [value, loading, error] = useCollection(q);
   // console.log('value ===', value);
   const docsWithUid =
     value && value.docs.map((doc) => ({ uid: doc.id, ...doc.data() }));
   useEffect(() => {}, []);
-  // console.log('docsWithUid ===', docsWithUid);
-  const filterTags = ({ tags }) => tags.includes(filerArr);
-  const filtered = docsWithUid?.filter(filterTags);
 
-  console.log('filtered ===', filtered);
   console.log('error ===', error);
   return (
     <div className="container">
       <h1>Post page</h1>
       <p>This is ProfilePage</p>
       <Loader show={loading} />
-      <button onClick={() => setFilerArr('boxing')}>boxing</button>
-      <button onClick={() => setFilerArr('kazkas')}>kazkas</button>
+      <button
+        className="btn btn-outline-dark btn-sm"
+        onClick={() => setFilerVal('all')}
+      >
+        all
+      </button>
+      <button
+        className="btn btn-outline-dark btn-sm"
+        onClick={() => setFilerVal('css')}
+      >
+        css
+      </button>
+      <button
+        className="btn btn-outline-dark btn-sm"
+        onClick={() => setFilerVal('boxing')}
+      >
+        boxing
+      </button>
+      <button
+        className="btn btn-outline-dark btn-sm"
+        onClick={() => setFilerVal('kazkas')}
+      >
+        kazkas
+      </button>
       <ul className="list-group">
         {value &&
-          docsWithUid.filter(filterTags).map((pObj) => (
+          docsWithUid.map((pObj) => (
             <li className="list-group-item list-group-item-dark" key={pObj.uid}>
               <SingleListPost item={pObj} />
             </li>
